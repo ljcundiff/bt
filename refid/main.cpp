c5c4eb3890ce77eae840508beaa6f51334437580
Lloyd Cundiff <ljcundiff@gmail.com>
2015-10-09 14:54:41 -0600

    added string headers

--- refid/main.cpp
+++ refid/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <boost/random.hpp>
 
 const char CHARS[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','2','3','4','5','6','7','8'};
--- replace/main.cpp
+++ replace/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int findLocation(std::string sourceFile, std::string searchText)
 {
